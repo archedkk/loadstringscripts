3d941d8d9c4b71093f01e505c4777c745fcb3d6f
archedkk <arckian@protonmail.com>
2024-08-05 21:22:39 +0100

    Update key.lua

--- tabbyresources/key.lua
+++ tabbyresources/key.lua
@@ -136,10 +136,8 @@ UIStroke_5.Parent = TextLabel_2
 
 	Submit.MouseButton1Click:Connect(function()
 		if Key.Text == "123.4" then
-		    getgenv().ama12ts = true
-		    wait(0.1)
                     keysys.Enabled = false
-                    loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/Tabby/main/TabbyPassedKey.lua", true))()
+                    loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/Tabby/main/TabbyPassedKey.lua", true))()(true)
 		    keysys:Destroy()
                  else
                     Submit.Text = "Wrong key!"
